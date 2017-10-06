e3bf2ecd594efc0fff5e8c20583d21f0eaa08c43
usuario <usuario@10.22.148.68>
2017-10-06 08:48:35 +0100

    Kata2, versión 1

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -11,9 +11,14 @@ public class Kata2 {
         
         Map<Integer,Integer> histogram = new HashMap<>();
 
-        for (int key : data) {
-            histogram.put(key, histogram.containsKey(key)?
-                            histogram.get(key)+1 : 1);
+        for (int i = 0; i < data.length; i++) {
+           
+            if (histogram.containsKey(data[i])) {
+                histogram.put(data[i],histogram.get(data[i])+1);
+                
+            }else{
+                histogram.put(data[i], 1);
+            }
             
         }
         for (int key: histogram.keySet()){
